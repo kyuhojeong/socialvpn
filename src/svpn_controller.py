bc34a962675333060b1993b84553e90102684800
Kyuho Jeong <kyuho.jeong@gmail.com>
2014-03-02 19:41:02 -0500

    multihop
    unknown destination packet is forwarded to controller. Then
    controller send multicast to peers (lookup request)  and if destination exists it
    replies. On reply message coming back to the source, routing
    information is registered each hop controllers' "farpeers" and
    data packet now can forwarded to destination (lookup reply).
    
    Now each controller runs TCP server and controllers' message can be
    sent and received through this interface. Currently, we assume multi
    hop is ad-hoc so does not consider much on overhead. TCP socket is
    is created for every packet, so port and packet are wasted on heavy
    traffic.
    
    This controller TCP connections are only IPv6 based.

--- src/svpn_controller.py
+++ src/svpn_controller.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import base64
 import binascii
 import getpass
 import hashlib
@@ -35,7 +36,7 @@ CONFIG = {
     "buf_size": 4096,
     "tincan_logging": 1,
     "controller_logging" : "INFO",
-    "multihop_mode": False
+    "multihop": False
 }
 
 IP_MAP = {}
@@ -63,6 +64,15 @@ def gen_ip6(uid, ip6=None):
         ip6 = CONFIG["ip6_prefix"]
     for i in range(0, 16, 4): ip6 += ":" + uid[i:i+4]
     return ip6
+
+def b2a_ip6(bin_ip6):
+    a_ip6=""
+    for i in range (0, 16, 2):
+         a_ip6 += bin_ip6[i:i+2].encode("hex")
+         if not i == 14:
+             a_ip6 += ":"
+    return a_ip6
+         
 
 def gen_uid(ip4):
     return hashlib.sha1(ip4).hexdigest()[:CONFIG["uid_size"]]
@@ -109,13 +119,15 @@ def do_get_state(sock):
 def do_set_logging(sock, logging):
     return make_call(sock, m="set_logging", logging=logging)
 
-def do_send_peers(sock, peers):
-    return make_call(sock, m="send_peers")
+def do_inject_to_channel(sock, src_uid, dest_uid, data):
+    b64=base64.b64encode(data)
+    return make_call(sock, m="inject_to_channel", src_uid=src_uid, dest_uid=dest_uid, data=b64)
 
 class UdpServer(object):
     def __init__(self, user, password, host, ip4, uid):
         self.state = {}
         self.peers = {}
+        self.far_peers = {}
         self.peerlist = set()
         self.ip_map = dict(IP_MAP)
 
@@ -126,11 +138,6 @@ class UdpServer(object):
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind(("", 0))
-        print uid
-        ip6 = gen_ip6(uid)
-        print ip6
-        self.tcp_sock.bind((ip6, CONFIG["tcp_port"]))
-        self.tcp_sock.listen(1)
 
         do_set_logging(self.sock, CONFIG["tincan_logging"])
         do_set_cb_endpoint(self.sock, self.sock.getsockname())
@@ -138,6 +145,30 @@ class UdpServer(object):
                         CONFIG["ip6_mask"], CONFIG["subnet_mask"])
         do_register_service(self.sock, user, password, host)
         do_get_state(self.sock)
+
+        if CONFIG["multihop"]:
+            if socket.has_ipv6:
+                self.tcp_sock = socket.socket(socket.AF_INET6, 
+                                              socket.SOCK_STREAM)
+            else:
+                self.tcp_sock = socket.socket(socket.AF_INET, 
+                                              socket.SOCK_STREAM)
+
+            #Wait until ipop tap device up
+            while True:
+                try:
+                    self.tcp_sock.bind((gen_ip6(uid), CONFIG["tcp_port"]))
+                except:
+                    time.sleep(1)
+                    continue
+                else:
+                    break
+
+            self.tcp_sock.listen(1)
+            self.sock_list = [self.sock, self.tcp_sock]
+            self.tcp_conn = None
+        else:
+            self.sock_list = [self.sock]
 
     def create_connection(self, uid, data, overlay_id, sec, cas, ip4):
         self.peerlist.add(uid)
@@ -150,28 +181,154 @@ class UdpServer(object):
                 if v["last_time"] > CONFIG["wait_time"] * 2:
                     do_trim_link(self.sock, k)
 
-    def serve(self):
-        socks = select.select([self.sock, self.tcp_sock], [], [], CONFIG["wait_time"])
-        print socks
-        print socks[0]
-        for sock in socks[0]:
-            data, addr = sock.recvfrom(CONFIG["buf_size"])
-            if data[0] == "{":
-                msg = json.loads(data)
-                logging.debug("recv %s %s" % (addr, data))
-                msg_type = msg.get("type", None)
+    def tcp_send(self, to, msg):
+        s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        s.connect((to, CONFIG["tcp_port"]))
+        s.send(msg)
+        s.close()
 
-                if msg_type == "local_state": self.state = msg
-                elif msg_type == "peer_state": self.peers[msg["uid"]] = msg
-                # we ignore connection status notification for now
-                elif msg_type == "con_stat": pass
-                elif msg_type == "con_req" or msg_type == "con_resp":
-                    fpr_len = len(self.state["_fpr"])
-                    fpr = msg["data"][:fpr_len]
-                    cas = msg["data"][fpr_len + 1:]
-                    ip4 = gen_ip4(msg["uid"], self.ip_map, self.state["_ip4"])
-                    self.create_connection(msg["uid"], fpr, 1, CONFIG["sec"],
-                                           cas, ip4)
+    def update_farpeers(self, key, hop_count, via):
+        if not key in self.far_peers:
+            self.far_peers[key] = {}
+            self.far_peers[key]["hop_count"] = sys.maxint
+        if self.far_peers[key]["hop_count"] >= hop_count:
+            self.far_peers[key]["hop_count"] = hop_count
+            self.far_peers[key]["via"] = via
+
+    def lookup(self, dest_ip6):
+        for k, v in self.peers.iteritems():
+            if "ip6" in v:
+                msg = json.dumps({"m":"lookup_request", "dest_ip6":dest_ip6, 
+                                  "via":[self.state["_ip6"], v["ip6"]]})
+                self.tcp_send(v["ip6"], msg)
+
+    def handle_lookup_request(self, msg):
+        for k, v in self.peers.iteritems():
+            # found in peer, do lookup_reply
+            if "ip6" in v and v["ip6"] == msg["dest_ip6"]:
+                # IP is found in my peers,  
+                # send reply message back to previous sender
+                jmsg = json.dumps({"m":"lookup_reply", 
+                       "dest_ip6":msg["via"][-2], "src_ip6":msg["dest_ip6"], 
+                       "src_uid":k, "via":msg["via"], 
+                       "hop_uid":self.state["_uid"], "hops":2})
+                self.tcp_send(msg["via"][-2], jmsg)
+                self.update_farpeers(msg["via"][0], len(msg["via"]), 
+                                     msg["via"][-2]) 
+                return
+
+        # dest not in my peer, multicasting to peers
+        for k, v in self.peers.iteritems():
+            #Do not send lookup_request back to source
+            if msg["via"][-2] == v["ip6"]:
+                continue
+
+            #If this message visit here before, just drop it
+            cyclic = False
+            for via in msg["via"]:
+                if v["ip6"] == via:
+                    cyclic = True
+                    break
+                 
+            #Multicast lookup_request
+            if not cyclic:
+                msg["via"].append(v["ip6"])
+                msg = json.dumps(msg)
+                self.tcp_send(v["ip6"], msg)
+
+    def handle_lookup_reply(self, msg):
+        # update far_peers if ip6 does not exists or shortest hop count
+        self.update_farpeers(msg["src_ip6"], msg["hops"], 
+                             msg["via"][-msg["hops"] + 1]) 
+        if msg["dest_ip6"] != self.state["_ip6"]:
+            self.tcp_send(hop, msg)
+        
+        for k, v in self.peers.iteritems():
+            if msg["dest_ip6"] == v["ip6"]:
+                return
+        self.update_farpeers(msg["dest_ip6"], len(msg["via"]) - msg["hops"], 
+                             msg["via"][len(msg["via"]) - msg["hops"]]) 
+
+    def serve(self):
+        socks, _, _ = select.select(self.sock_list, [], [], CONFIG["wait_time"])
+        for sock in socks:
+            if sock == self.sock:
+                data, addr = sock.recvfrom(CONFIG["buf_size"])
+                # TODO maybe we should put header to distinguish actual data 
+                # packet and controller message
+                if data[0] == "{" and data[-2] == "}" and data[-1] == "\n":
+                    msg = json.loads(data)
+                    logging.debug("recv %s %s" % (addr, data))
+                    msg_type = msg.get("type", None)
+
+                    if msg_type == "local_state": self.state = msg
+                    elif msg_type == "peer_state": 
+                        if "far_peers" in self.peers:
+                            msg["far_peers"] = \
+                                   self.peers[msg["uid"]]["far_peers"]
+                        self.peers[msg["uid"]] = msg
+                    # we ignore connection status notification for now
+                    elif msg_type == "con_stat": pass
+                    elif msg_type == "con_req" or msg_type == "con_resp":
+                        fpr_len = len(self.state["_fpr"])
+                        fpr = msg["data"][:fpr_len]
+                        cas = msg["data"][fpr_len + 1:]
+                        ip4 = gen_ip4(msg["uid"], self.ip_map, 
+                                      self.state["_ip4"])
+                        self.create_connection(msg["uid"], fpr, 1, 
+                                               CONFIG["sec"], cas, ip4)
+                else:
+                    # 0:20 source uid
+                    # 20:40 destination uid
+                    # 40:46 destination mac
+                    # 46:52 source mac
+                    # 52:53 ether type
+                    # we don't handle 802.1Q at this point
+         
+                    #packet is ipv6 and not multicast
+                    if data[52:54] == "\x86\xdd" and data[78:80] != "\xff\x02":
+                        dest_ip6=b2a_ip6(data[78:94])
+                        logging.debug("non-peer destination({0}) packet is" 
+                                                " forwarded".format(dest_ip6))
+                        if dest_ip6 in self.far_peers:
+                            self.tcp_send(self.far_peers[dest_ip6]["via"], 
+                                          data[40:])
+                        else:
+                            self.lookup(dest_ip6)
+
+            if not CONFIG["multihop"]:
+                return
+             
+            if sock == self.tcp_sock:
+                self.tcp_conn, addr = sock.accept()
+                logging.debug("TCP Connection from {0}".format(addr))
+                self.sock_list.append(self.tcp_conn)
+
+            if sock == self.tcp_conn:
+                data = sock.recv(CONFIG["buf_size"]);
+                if not data:
+                    self.sock_list.remove(self.tcp_conn)
+                    return
+
+                if data[0] == "{" and data[-1] == "}":
+                    msg = json.loads(data)
+                    logging.debug("Control message arrived {0}".format(msg))
+                    if msg["m"] == "lookup_request":
+                        self.handle_lookup_request(msg)
+                    elif msg["m"] == "lookup_reply":
+                        self.handle_lookup_reply(msg)
+                else:
+                    dest_ip6=b2a_ip6(data[38:54])
+                    logging.debug("Traffic packet destined to {0}" 
+                                  "arrived".format(dest_ip6))
+                    for k, v in self.peers.iteritems():
+                        if v["ip6"] == dest_ip6:
+                            do_inject_to_channel(self.sock, self.state["_uid"], 
+                                                 k, data)
+                            return
+                      
+                    if dest_ip6 in self.far_peers:
+                        self.tcp_send(self.far_peers[dest_ip6]["via"], data)
 
 def setup_config(config):
     """Validate config and set default value here. Return ``True`` if config is
@@ -244,8 +401,6 @@ def main():
             count += 1
             server.trim_connections()
             do_get_state(server.sock)
-            if CONFIG["multihop_mode"] :
-                do_send_peers(server.sock)
             last_time = time.time()
 
 if __name__ == "__main__":
